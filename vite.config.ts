6832b94cb1a413d73c16deebc2f45050b9780417
kartik parmar <83116676+kartik-parmar007@users.noreply.github.com>
2026-06-06 10:37:33 +0530

    vercel error

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    nitro(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
