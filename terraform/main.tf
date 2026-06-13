706f9385e858bf5b0e203129eeb0b72888da6c5e
kartik parmar <83116676+kartik-parmar007@users.noreply.github.com>
2026-06-13 19:56:27 +0530

    fix: format main.tf using terraform fmt and ignore terraform files in prettier

--- terraform/main.tf
+++ terraform/main.tf
@@ -187,12 +187,12 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 # 10. EC2 Server (AWS Free Tier - t2.micro)
 resource "aws_instance" "web_server" {
-  ami                  = var.ami_id
-  instance_type        = "t2.micro" # Free Tier eligible
-  subnet_id            = aws_subnet.public_subnet.id
+  ami                    = var.ami_id
+  instance_type          = "t2.micro" # Free Tier eligible
+  subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name             = aws_key_pair.generated_key.key_name
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  key_name               = aws_key_pair.generated_key.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   # Setup script to automatically install Docker, Docker Compose, and AWS CLI
   user_data = <<-EOF
